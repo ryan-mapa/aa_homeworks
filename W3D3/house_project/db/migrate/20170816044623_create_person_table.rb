fdcb087656e6ebd5f65c123a5fbb1f4955ac6b54
ryan mapa <ryan.m.mapa@gmail.com>
2017-08-16 08:53:56 -0700

    Please work

---
+++ W3D3/house_project/db/migrate/20170816044623_create_person_table.rb
@@ -0,0 +1,9 @@
+class CreatePersonTable < ActiveRecord::Migration[5.1]
+  def change
+    create_table :people do |t|
+      t.string :name, null: false
+      t.integer :house_id
+      t.timestamps
+    end
+  end
+end
