f34a9152126a5b303846730ad7dc6a87b5953add
ryan mapa <ryan.m.mapa@gmail.com>
2017-08-24 20:29:10 -0700

    Add hw w4d5

--- W4D4/first_auth/app/models/user.rb
+++ W4D4/first_auth/app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   attr_reader :password
 
   def self.generate_session_token
-    SecureRandom::urlsafe_base64(16)
+    SecureRandom::urlsafe_base64(16) #urlsafe_base64 NOT urlsave_base64
   end
 
   def ensure_session_token
@@ -31,25 +31,23 @@ class User < ApplicationRecord
     nil #not found
   end
 
-
-
-  def password=(password)
+  def password=(password) #creates digest upon password creation
     @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      render json: @user
-    else
-      render json: @user.errors.full_messages
-    end
-  end
-
-  def new
-    @user = User.new
-  end
+  # def create
+  #   @user = User.new(user_params)
+  #   if @user.save
+  #     render json: @user
+  #   else
+  #     render json: @user.errors.full_messages
+  #   end
+  # end
+  #
+  # def new
+  #   @user = User.new
+  # end
 
   private
 
