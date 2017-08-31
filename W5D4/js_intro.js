4b4891cc57695b4ce5884b1196f1745a7250eaa3
ryan mapa <ryan.m.mapa@gmail.com>
2017-08-31 08:24:12 -0700

    Add w5d4 hw

---
+++ W5D4/js_intro.js
@@ -0,0 +1,50 @@
+function madlib (verb, adj, noun) {
+  console.log('We shall ${verb} the ${adj} ${noun}.');
+}
+
+
+function isSubstring(string, sub) {
+  return string.includes(sub);
+}
+
+function fizzBuzz (array) {
+  const fizzBuzzArr = [];
+
+  array.forEach(el => {
+    if ((el % 3 === 0) ^ (el % 5 === 0)) {
+      fizzBuzzArr.push(el);
+    }
+  });
+
+  return fizzBuzzArr;
+}
+
+
+function isPrime (n) {
+  if (n < 2) { return false; }
+
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+
+function sumOfNPrimes (n) {
+  let sum = 0;
+  let countPrimes = 0;
+  let i = 2;
+
+  while (countPrimes < n) {
+    if (isPrime(i)) {
+      sum += i;
+      countPrimes++;
+    }
+    i++;
+  }
+
+  return sum;
+}
