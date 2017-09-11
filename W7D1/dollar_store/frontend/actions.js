f5c4a075ee9fd2c91c255469112fb94f370bb039
ryan mapa <ryan.m.mapa@gmail.com>
2017-09-10 18:36:09 -0700

    Complete dollar store

---
+++ W7D1/dollar_store/frontend/actions.js
@@ -0,0 +1,12 @@
+
+
+const selectCurrency = ({baseCurrency, rates}) => ({
+  type: "SWITCH_CURRENCY",
+  baseCurrency,
+  rates
+});
+
+export default selectCurrency;
+
+
+// window.selectCurrency = selectCurrency;
